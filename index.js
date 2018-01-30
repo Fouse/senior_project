6e79679f95957dd0ede355534006564fd5998438
adschaf <adschaf@pointpark.edu>
2018-01-29 20:49:37 -0500

    Added 404, 500, and db connection

--- index.js
+++ index.js
@@ -21,3 +21,47 @@ app.get("/user", function(req, res) {
 app.listen(app.get('port'), function() {
         console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });
+
+
+
+/* DB Connection
+ * USE connect(function(con){}); inside POST to call DB
+*/
+var con = mysql.createConnection({
+    host : 'db.it.pointpark.edu',
+    user : 'pointlift',
+    password : 'pointlift',
+    database : 'db.it.pointpark.edu'
+});
+
+function connect(cb){
+  if(con.state === 'disconnected'){
+
+    con.connect(function(err){
+      if (err){
+        console.log('error: ' + err.stack);
+        return;
+      }
+      cb(con);
+    });
+  } else {
+    cb(con);
+  }
+}
+
+
+//custom 404 page
+app.use(function(req, res){
+  res.status(404);
+  res.render("404");
+});
+
+//custom 500 page
+app.use(function(err, req, res, next){
+  console.log(err.stack);
+  res.status(500);
+  res.render("500");
+});
+
+
+
