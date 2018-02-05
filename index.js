932c66deae5ef7f4fb5310b4b41795b412360f6c
adschaf <adschaf@pointpark.edu>
2018-02-05 17:11:56 -0500

    Added Link Routes

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:"main"});
+var mysql = require('mysql');
 
 app.engine("handlebars",handlebars.engine);
 app.set("view engine","handlebars");
 app.set('port', process.env.PORT || 3000);
-
-app.get("/", function(req, res) {
-	res.render("home");
-});
-
-app.get("/admin", function(req, res) {
-        res.render("admin");
-});
-
-app.get("/user", function(req, res) {
-        res.render("user");
-});
-
-app.listen(app.get('port'), function() {
-        console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
-});
-
 
 
 /* DB Connection
@@ -49,6 +33,47 @@ function connect(cb){
   }
 }
 
+function getMenu(req){
+  var menu =[];
+//  var isAdmin = req.session.is_admin;
+   menu.push({"page": "/", "label": "Home"},{"page": "about", "label": "About"});
+
+//  if(isAdmin){
+//    menu.push({"page": "admin", "label": "My Requests"});
+//  } else{
+//    if(req.session.user_id){
+//    } else {
+//      menu.push({"page": "addUser", "label": "Sign Up"},{"page": "home-login", "label":
+//    }
+//  }
+
+  return menu;
+};
+
+
+app.get("/", function(req, res) {
+  res.render("home", {
+  menu: getMenu(req)
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+  menu: getMenu(req)
+  });
+});
+
+app.get("/admin", function(req, res) {
+  res.render("admin", {
+  menu: getMenu(req)
+  });
+});
+
+app.get("/user", function(req, res) {
+  res.render("user", {
+  menu: getMenu(req)
+  });
+});
 
 //custom 404 page
 app.use(function(req, res){
@@ -63,5 +88,6 @@ app.use(function(err, req, res, next){
   res.render("500");
 });
 
-
-
+app.listen(app.get('port'), function() {
+  console.log("Express started on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
+});
